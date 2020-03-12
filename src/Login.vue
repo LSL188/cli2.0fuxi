8b66c64b6d9ca39ad99e0db317758ace0199e091
刘素莉 <1114317678@qq.com>
2020-03-12 11:54:52 +0800

    init base-project

---
+++ src/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        login
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
